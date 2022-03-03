a3ca40e432d017e79a30256c7d41c4e9147aceeb
Bhavika Setpal <bhavikasetpal123@gmail.com>
2022-03-03 13:17:20 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactiveform';
+
+  submitForm= new FormGroup({
+    user:new FormControl(''),
+    email:new FormControl(''),
+    password:new FormControl('')
+  })
+  submituser()
+  {
+    console.warn(this.submitForm.value)
+  }
 }
